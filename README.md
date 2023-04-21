59df90b69b5f6cfb9014659d6a11a0d241263b2e
traven <traven.shih@infineon.com>
2023-04-21 11:11:11 +0800

    output timestamp instead of line

--- README.md
+++ README.md
@@ -18,41 +18,38 @@ C:\> py chk_hp_lps_csv.py -f <folder> -c <CC channel>
 Parsing....
 
         > 230304\20230103_Varcolac_NoCharge_OCI1.0.4.16.0_Fail.sal
-          Channel 8 doesn't exist, scan others....
-                No 100W: 1       [763]
+          No 100W: 1     [2.45433s]
         > 230304\20230103_Varcolac_NoCharge_OCI1.0.4.16.0_Pass.sal
-                HP  LPS: 1       [795]
+          HP  LPS: 1     [3.48341s]
         > 230304\20230110_Varcolac_NoChargeIssue_PD_bootleg2.3.00.08_Fail.sal
-                No 100W: 1       [763]
+          No 100W: 1     [4.44714s]
         > 230304\20230110_Varcolac_NoChargeIssue_PD_bootleg2.3.00.08_Pass.sal
-                HP  LPS: 1       [795]
+          HP  LPS: 1     [3.68401s]
         > 230304\230303_110934_hard_reset.sal
-          Channel 9 is not CC, scan others....
-                HP  LPS: 5       [763, 1828, 2851, 3901, 4924]
-                Not 75W: 1       [4979]
+          HP  LPS: 5     [4.08425s, 39.1473s, 74.0997s, 109.193s, 144.395s]
+          Not 75W: 1     [145.996s]
         > 230304\230304_074727_100Wseveral times.sal
-                HP  LPS: 5       [830, 1812, 2903, 3952, 5601]
-                Not 75W: 1       [4007]
+          HP  LPS: 5     [3.81414s, 38.8838s, 74.0974s, 109.111s, 143.917s]
+          Not 75W: 1     [110.714s]
         > 230304\230310_082610_corrupt.sal
-          File might be corrupted
+                File might be corrupted
         > 230304\230312_010125.sal
-          Channel 12 is not CC, scan others....
-                HP  LPS: 5       [790, 1909, 2944, 4021, 5098]
+          HP  LPS: 5     [5.26322s, 50.5649s, 95.4695s, 140.305s, 185.515s]
         > 230304\230312_010250.sal
-                HP  LPS: 4       [790, 1867, 3020, 4021]
+          HP  LPS: 4     [4.94915s, 50.0130s, 95.3241s, 185.419s]
         > 230304\230312_010530.sal
-                HP  LPS: 4       [790, 1867, 2944, 4021]
+          HP  LPS: 4     [50.1081s, 95.1457s, 140.485s, 185.249s]
         > 230304\230312_010654.sal
-                HP  LPS: 5       [897, 2001, 2998, 4099, 5176]
+          HP  LPS: 5     [5.04617s, 50.3376s, 95.3430s, 140.364s, 185.308s]
         > 230304\230312_010934.sal
-                HP  LPS: 4       [790, 1867, 3051, 4062]
+          HP  LPS: 4     [50.5110s, 95.5775s, 140.480s, 185.404s]
         > 230304\230312_011059.sal
-                HP  LPS: 4       [832, 1867, 2944, 4021]
+          HP  LPS: 4     [5.31648s, 95.2046s, 140.184s, 185.417s]
 
 Total Pass: 38
 Total Fail: 4 ( Not 75W x 2 ; No 100W x 2 )
 Total 12 logs, 42 test cycles
-Total Time 1 min(s) 9 sec(s)
+Total Time 1 min(s) 27 sec(s)
 ========================================================================
 ```
 
--- chk_hp_lps_csv.py
+++ chk_hp_lps_csv.py
@@ -1,62 +1,77 @@
-from collections import defaultdict
-from typing import List
 from saleae import automation
-
 import argparse
-import ctypes
 import sys
 import os
 import os.path
 import glob
 import time
+import csv
 
 export_path = os.getcwd()
 args=None
 
+# use Manchester_PD_CC for parsing timestamp
+DEFINE_ORI_ANALYSER=1
+
+# strings defination for strings
+DEFINE_75W =           ["| Fix [ 20000mV | 3750mA ]", "Fix supply [Volt =  20000mV | Curr =  3750mA"]
+DEFINE_100W =          ["| Fix [ 20000mV | 5000mA ]", "Fix supply [Volt =  20000mV | Curr =  5000mA"]
+DEFINE_SNK_CAP_EXT =   ["Snk_Cap_Ext	   SNK", "Sink_Capabilities_Extended"]
+DEFINE_HARD_RESET =    ["HARD_RESET"]
+
+# output timestamp length 
+DEFINE_TIMESTAMP_LENGTH = 7
+
 def parseArguments ():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-f', required=True, help="",
-            metavar="<input dir>",dest='input_dir')
-    parser.add_argument('-c', required=True, help="",
-            metavar="<analyser channel>",dest='channel')
+    parser.add_argument('-f', required=True, help="", metavar="<input dir>",dest='input_dir')
+    parser.add_argument('-c', required=True, help="", metavar="<analyser channel>",dest='channel')
+    #parser.add_argument('-t', dest='define_timestamp', action='store_true', help="use -t to print results with timestamp")
+    #parser.add_argument('-l', dest='define_linenumber', action='store_true', help="use -l to print results with line number")
 
     return parser.parse_args()
     # ...
 
 def channel_autoscan(output_file,success_count,no_100w_count,not_75w_count):
-    #re-scan all channels if assigned channel is incorrect
     start_channel_no = 0
     while success_count+no_100w_count+not_75w_count == 0:
-        #print("\t>",file_path,"\r\n\t\tNo result, could be wrong CC channel assignment, retrying....")
-        
+        # re-scan all channels if assigned channel is incorrect
         for chk_channel in range(start_channel_no, 16):
             try:
-                cc_analyzer = capture.add_analyzer('Saleae_PD_CC', settings={
-                    'Manchester': chk_channel,
-                    'Bit Rate (Bits/s)': 1,
-                })
+                if DEFINE_ORI_ANALYSER:
+                    cc_analyzer = capture.add_analyzer('Manchester_PD_CC', settings={
+                        'Manchester': chk_channel,
+                    })
+                else:
+                    cc_analyzer = capture.add_analyzer('Saleae_PD_CC', settings={
+                        'Manchester': chk_channel,
+                        'Bit Rate (Bits/s)': 1,
+                    })
                 capture.export_data_table(filepath=export_filepath, analyzers=[cc_analyzer], columns=['Start','value'])
             
                 #parse HP LPS flow
-                success_count, success_lines, no_100w_count, no_100w_lines, not_75w_count, not_75w_lines = check_file(output_file)
+                success_count, success_seconds, no_100w_count,\
+                no_100w_seconds, not_75w_count, not_75w_seconds = check_file(output_file)
                 
+                # break and return channel number which is CC one
                 if success_count+no_100w_count+not_75w_count !=0:
                     new_valid_channel = chk_channel
                     break
             except:
+                # channel doesn't exist, skip to next one
                 start_channel_no = chk_channel+1
                 break
 
-    return success_count, success_lines, no_100w_count, no_100w_lines, not_75w_count, not_75w_lines, new_valid_channel
+    return success_count, success_seconds, no_100w_count, no_100w_seconds, not_75w_count, not_75w_seconds, new_valid_channel
 
 def check_file(filepath):
     success_count = 0
-    success_lines = []
     not_75w_count = 0
-    not_75w_lines = []
     no_100w_count = 0
-    no_100w_lines = []
-    snk_cap_ext_line = 0
+    success_seconds = []
+    not_75w_seconds = []
+    no_100w_seconds = []
+    snk_cap_ext_seconds = []
 
     with open(filepath, 'r') as file:
         lines = file.readlines()
@@ -68,69 +83,67 @@ def check_file(filepath):
             sent_snk_cap_ext = False
             another_round = False
             
-            if "| Fix [ 20000mV | 3750mA ]" in lines[i]:
+            if any(s in lines[i] for s in DEFINE_75W):
                 start_as_75w = True
 
                 for j in range(i+1, len(lines)):
-                    if "| Fix [ 20000mV | 3750mA ]" in lines[j]:
+                    if any(s in lines[j] for s in DEFINE_75W):
                         i = j - 5
                         break
-                    if "HARD_RESET" in lines[j]:
+                    if any(s in lines[j] for s in DEFINE_HARD_RESET):
                         i = j - 1
                         break
                     else:
-                        if "| Fix [ 20000mV | 5000mA ]" in lines[j]:
+                        if any(s in lines[j] for s in DEFINE_100W):
                             rise_to_100w = True
-                            rise_to_100w_line = j + 1
                             # finish check in this range. add 1 to avoid repeatedly check
                             i = j + 1
                             break
-                        if "Snk_Cap_Ext	" in lines[j]:
+                        if any(s in lines[j] for s in DEFINE_SNK_CAP_EXT):
                             sent_snk_cap_ext = True
-                            snk_cap_ext_line = j + 1
-                        if "| Fix [ 20000mV | 3750mA ]" in lines[j]:
+                        if any(s in lines[j] for s in DEFINE_75W):
                             another_round = True
-                            another_round_line = j + 1
                             i = j
                             break
                             
-            elif "| Fix [ 20000mV | 5000mA ]" in lines[i]:
+            elif any(s in lines[i] for s in DEFINE_100W):
                 start_as_75w = False
-                not_75w_start_line = i + 1
                 for j in range(i+1, len(lines)):
-                    if "| Fix [ 20000mV | 3750mA ]" in lines[j]:
+                    if any(s in lines[j] for s in DEFINE_75W):
                         another_round = True
                         i = j
                         break
-                    if "| Fix [ 20000mV | 5000mA ]" in lines[j]:
+                    if any(s in lines[j] for s in DEFINE_100W):
                         another_round = True
                         i = j
                         break
-                    if "HARD_RESET" in lines[j]:
+                    if any(s in lines[j] for s in DEFINE_HARD_RESET):
                         another_round = True
                         i = j
                         break
-                         
+            
+            # start with 75W, complete LPS check flow and rise to 100W
             if start_as_75w and sent_snk_cap_ext and rise_to_100w:
                 success_count +=1
-                success_lines.append(rise_to_100w_line)
+                # found target and record time value at previous csv column 
+                success_seconds.append(lines[i-1].split(',')[1].strip())
             elif start_as_75w and sent_snk_cap_ext and rise_to_100w and not file.readlines():
                 success_count +=1
-                success_lines.append(rise_to_100w_line)
+                success_seconds.append(lines[i-1].split(',')[1].strip())
             elif start_as_75w and sent_snk_cap_ext and not rise_to_100w and another_round:
                 no_100w_count +=1
-                no_100w_lines.append(snk_cap_ext_line)
+                no_100w_seconds.append(lines[i-1].split(',')[1].strip())
             elif start_as_75w and sent_snk_cap_ext and not rise_to_100w and not file.readlines():
                 no_100w_count +=1
-                no_100w_lines.append(snk_cap_ext_line)
+                no_100w_seconds.append(lines[i-1].split(',')[1].strip())
                 
             elif not start_as_75w and another_round:
                 not_75w_count +=1
-                not_75w_lines.append(not_75w_start_line)
+                not_75w_seconds.append(lines[i-1].split(',')[1].strip())
                 
             i += 1
 
-    return success_count, success_lines, no_100w_count, no_100w_lines, not_75w_count, not_75w_lines
+    return success_count, success_seconds, no_100w_count, no_100w_seconds, not_75w_count, not_75w_seconds
 
 if __name__ == "__main__":
 
@@ -159,7 +172,7 @@ if __name__ == "__main__":
     for file_path in sal_files: 
         salfile_path = os.path.join(os.getcwd(), args.input_dir)
         salfile_path = os.path.join(salfile_path, os.path.basename(file_path))
-        output_file = salfile_path+'.txt'
+        output_file = salfile_path+'.csv'
         export_filepath = os.path.join(export_path, output_file)
         
         try:
@@ -167,46 +180,65 @@ if __name__ == "__main__":
                 print("\t>",file_path)
                 # Add an analyzer to the capture
                 try:
-                    cc_analyzer = capture.add_analyzer('Saleae_PD_CC', settings={
-                        'Manchester': last_valid_channel,
-                        'Bit Rate (Bits/s)': 1,
-                    })
+                    if DEFINE_ORI_ANALYSER:
+                        cc_analyzer = capture.add_analyzer('Manchester_PD_CC', settings={
+                            'Manchester': last_valid_channel,
+                        })
+                    else:
+                        cc_analyzer = capture.add_analyzer('Saleae_PD_CC', settings={
+                            'Manchester': last_valid_channel,
+                            'Bit Rate (Bits/s)': 1,
+                        })
+                    # export to csv
                     capture.export_data_table(filepath=export_filepath, analyzers=[cc_analyzer], columns=['Start','value'])
                 
-                    #parse HP LPS flow
-                    success_count, success_lines, no_100w_count, no_100w_lines, not_75w_count, not_75w_lines = check_file(output_file)
+                    # parse HP LPS flow
+                    success_count, success_seconds, no_100w_count,\
+                    no_100w_seconds, not_75w_count, not_75w_seconds = check_file(output_file)
                     
+                    # no result, maybe assign wrong CC channel
                     if success_count+no_100w_count+not_75w_count == 0:
-                        print("\t  Channel",last_valid_channel,"is not CC, scan others....")
-                        success_count, success_lines, no_100w_count, no_100w_lines, not_75w_count, not_75w_lines, last_valid_channel = channel_autoscan(output_file,success_count,no_100w_count,not_75w_count)
-                    
+                        #print("\t  Channel",last_valid_channel,"is not CC, scan others....", end='')
+                        # search correct CC channel
+                        success_count, success_seconds, no_100w_count,\
+                        no_100w_seconds, not_75w_count, not_75w_seconds,\
+                        last_valid_channel = channel_autoscan(output_file,success_count,no_100w_count,not_75w_count)
+                        #print("found at channel", last_valid_channel)
                 except:
-                    print("\t  Channel",last_valid_channel,"doesn't exist, scan others....")
-                    success_count, success_lines, no_100w_count, no_100w_lines, not_75w_count, not_75w_lines, last_valid_channel = channel_autoscan(output_file,success_count,no_100w_count,not_75w_count)
-
-                if success_count > 0 and no_100w_count == 0 and not_75w_count == 0:
-                    print("\t\tHP  LPS:",success_count,"\t", success_lines)
+                    # assigned channel doesn't exist in log file.
+                    #print("\t  Channel",last_valid_channel,"doesn't exist, scan others....", end='')
+                    # search other channels
+                    success_count, success_seconds, no_100w_count,\
+                    no_100w_seconds, not_75w_count, not_75w_seconds,\
+                    last_valid_channel = channel_autoscan(output_file,success_count,no_100w_count,not_75w_count)
+                    #print("found at channel", last_valid_channel)
                 
+                # all pass in log
+                if success_count > 0 and no_100w_count == 0 and not_75w_count == 0:
+                    # re-format success_seconds array into fixed string length, add ',' and print
+                    print("\t  HP  LPS:",success_count,"\t ["+", ".join([s[:DEFINE_TIMESTAMP_LENGTH]+'s' for s in success_seconds])+"]")
                 else:
                     if success_count != 0:
-                        print("\t\tHP  LPS:", success_count,"\t",success_lines)
-                    #else:
-                        #print("\t>",file_path)
+                        print("\t  HP  LPS:", success_count,"\t ["+", ".join([s[:DEFINE_TIMESTAMP_LENGTH]+'s' for s in success_seconds])+"]")
                     if no_100w_count !=0:
-                        print("\t\tNo 100W:", no_100w_count,"\t",no_100w_lines)
+                        print("\t  No 100W:", no_100w_count,"\t ["+", ".join([s[:DEFINE_TIMESTAMP_LENGTH]+'s' for s in no_100w_seconds])+"]")
                     if not_75w_count !=0:
-                        print("\t\tNot 75W:",not_75w_count,"\t",not_75w_lines)
-                
+                        print("\t  Not 75W:", not_75w_count,"\t ["+", ".join([s[:DEFINE_TIMESTAMP_LENGTH]+'s' for s in not_75w_seconds])+"]")
+                        
                 total_pass_count += success_count
                 total_not_75w += not_75w_count
                 total_not_100w += no_100w_count
                 total_parse_files += 1
+                # delete exported csv file after parsing files
+                os.remove(output_file)
         except:
-            print("\t>",file_path,"\r\n\t  File might be corrupted")
+            # fail in assigning analyser in log file. 
+            print("\t>",file_path,"\r\n\t\tFile might be corrupted")
         
     manager.close()
     end_time = time.time()    
     
+    #print summary
     print("\r\nTotal Pass:", total_pass_count)
     if total_not_75w+total_not_100w == 0:
         print("Total Fail:", total_not_75w+total_not_100w)
